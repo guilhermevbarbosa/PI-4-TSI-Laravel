7f51ca784b1713368925dd2c2cbb502b941f67be
Guilherme Vinicius Barbosa <31224718+guilhermevbarbosa@users.noreply.github.com>
2020-09-24 21:11:54 -0300

    make controller products
    - php artisan make:controller ProductsController --resource
    
    ROUTES WEB
    
    -use App\Http\Controllers\ProductsController;
    -Route::resource('/products', ProductsController::class);

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\ProductsController;
+
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::resource('/products', ProductsController::class);
