7fc749676a020c89a27b1b720de790172386cfe9
Guilherme Vinicius Barbosa <31224718+guilhermevbarbosa@users.noreply.github.com>
2020-10-06 22:13:02 -0300

    organizando rotas

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\CategoriesController;
 
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/products', ProductsController::class);
-Route::resource('/categories', CategoriesController::class);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::resource('/products', ProductsController::class);
+    Route::resource('/categories', CategoriesController::class);
+});
